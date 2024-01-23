a41e992eb0f330649d62a26dee053394989ffecb
Thomas <viltetomas2003@gmail.com>
2024-01-22 21:50:27 -0300

    create project sprint boot

---
+++ peliculas-app/src/main/java/com/peliculas/peliculasapp/PeliculasAppApplication.java
@@ -0,0 +1,13 @@
+package com.peliculas.peliculasapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeliculasAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeliculasAppApplication.class, args);
+	}
+
+}
---
+++ peliculas-app/src/test/java/com/peliculas/peliculasapp/PeliculasAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peliculas.peliculasapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeliculasAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
