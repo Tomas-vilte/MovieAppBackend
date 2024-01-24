bcc305e7b1348b618b09818f5871509c350c380d
Thomas <viltetomas2003@gmail.com>
2024-01-24 20:13:19 -0300

    add service

---
+++ peliculas-app/src/main/java/com/peliculas/peliculasapp/service/UserService.java
@@ -0,0 +1,22 @@
+package com.peliculas.peliculasapp.service;
+import com.peliculas.peliculasapp.models.User;
+import com.peliculas.peliculasapp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+}
