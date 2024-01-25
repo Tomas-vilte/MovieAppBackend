e8792b9e4b7d7514dfc90cf959bd486671fb154f
Thomas <viltetomas2003@gmail.com>
2024-01-25 15:08:09 -0300

    implemente controller

--- peliculas-app/src/main/java/com/peliculas/peliculasapp/controller/ReviewController.java
+++ peliculas-app/src/main/java/com/peliculas/peliculasapp/controller/ReviewController.java
@@ -1,4 +1,25 @@
 package com.peliculas.peliculasapp.controller;
 
+
+import com.peliculas.peliculasapp.models.Review;
+import com.peliculas.peliculasapp.repository.ReviewRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/reviews")
 public class ReviewController {
+
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    @GetMapping("/byContentId/{contentId}")
+    public List<Review> getReviewsByContentId(@PathVariable Integer contentId) {
+        return reviewRepository.getReviewsByContentId(contentId);
+    }
 }
