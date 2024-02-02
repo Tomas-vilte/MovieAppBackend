e1fdd098326195c84ad8e880fa04dafd00f8bfb9
Thomas <viltetomas2003@gmail.com>
2024-02-01 21:46:11 -0300

    Agrega configuración de API y adaptador para ExternalServicePort

--- peliculas-app/src/main/java/com/peliculas/peliculasapp/infrastructure/adapters/MovieDetailsAdapter.java
+++ peliculas-app/src/main/java/com/peliculas/peliculasapp/infrastructure/adapters/MovieDetailsAdapter.java
@@ -1,7 +1,33 @@
 package com.peliculas.peliculasapp.infrastructure.adapters;
+import com.peliculas.peliculasapp.domain.models.Movie;
+import com.peliculas.peliculasapp.domain.models.TvSeries;
+import com.peliculas.peliculasapp.domain.ports.out.ExternalServicePort;
+import com.peliculas.peliculasapp.infrastructure.config.ApiConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 @Component
-public class MovieDetailsAdapter {
+public class MovieDetailsAdapter implements ExternalServicePort {
+
+    private final ApiConfiguration apiConfiguration;
+
+    public MovieDetailsAdapter(ApiConfiguration apiConfiguration) {
+        this.apiConfiguration = apiConfiguration;
+    }
+
+
+    @Override
+    public Movie getMovieInfoById(long movieId) {
+        String endpoint = apiConfiguration.getApiUrl() + "movie/" + movieId + "?language=es-MX";
+        return new RestTemplate().getForObject(endpoint, Movie.class);
+    }
+
+
+    @Override
+    public TvSeries getTvSeriesInfoById(long tvSeriesId) {
+        String endpoint = apiConfiguration.getApiUrl() + "tv/" + tvSeriesId + "?language=es-MX";
+        return new RestTemplate().getForObject(endpoint, TvSeries.class);
+    }
 
 }
