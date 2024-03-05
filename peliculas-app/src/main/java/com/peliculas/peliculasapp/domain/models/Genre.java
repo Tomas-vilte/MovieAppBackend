cef8e442dda05a8add99d9733854c2d3e6a53c73
Thomas <viltetomas2003@gmail.com>
2024-03-05 16:53:11 -0300

    add annotation

--- peliculas-app/src/main/java/com/peliculas/peliculasapp/domain/models/Genre.java
+++ peliculas-app/src/main/java/com/peliculas/peliculasapp/domain/models/Genre.java
@@ -1,15 +1,10 @@
 package com.peliculas.peliculasapp.domain.models;
 
+import lombok.Data;
+
+@Data
 public class Genre {
     private long id;
 
     private String name;
-
-    public long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
 }
--- peliculas-app/src/main/java/com/peliculas/peliculasapp/dto/MovieDTO.java
+++ peliculas-app/src/main/java/com/peliculas/peliculasapp/dto/MovieDTO.java
@@ -1,20 +1,14 @@
 package com.peliculas.peliculasapp.dto;
 import com.peliculas.peliculasapp.domain.models.Genre;
-import lombok.*;
-
-import java.util.Date;
+import lombok.Data;
 import java.util.List;
 
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
 public class MovieDTO {
     private String title;
     private String overview;
-    private Date release_date;
+    private String release_date;
     private List<Genre> genres;
     private float popularity;
 }
