c742eb0a76ddf27ecb49120639cb5e45ce747937
Thomas <viltetomas2003@gmail.com>
2024-03-09 13:46:00 -0300

    add attributes LastEpisode

--- peliculas-app/src/main/java/com/peliculas/peliculasapp/domain/models/NextEpisode.java
+++ peliculas-app/src/main/java/com/peliculas/peliculasapp/domain/models/NextEpisode.java
@@ -1,5 +1,17 @@
 package com.peliculas.peliculasapp.domain.models;
 
 public class NextEpisode {
-    // TODO: Add attributes
+    private int id;
+    private String name;
+    private String overview;
+    private float vote_average;
+    private int vote_count;
+    private String air_date;
+    private int episode_number;
+    private String episode_type;
+    private String production_code;
+    private int runtime;
+    private int season_number;
+    private int show_id;
+    private String still_path;
 }
