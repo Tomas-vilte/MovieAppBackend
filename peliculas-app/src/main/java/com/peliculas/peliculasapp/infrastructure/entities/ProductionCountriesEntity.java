55687f4f9123ace413b63b71204702a6f055057b
Thomas <viltetomas2003@gmail.com>
2024-03-13 13:43:42 -0300

    implement method of ProductionCountriesEntity

--- peliculas-app/src/main/java/com/peliculas/peliculasapp/infrastructure/entities/ProductionCountriesEntity.java
+++ peliculas-app/src/main/java/com/peliculas/peliculasapp/infrastructure/entities/ProductionCountriesEntity.java
@@ -1,26 +1,36 @@
-    package com.peliculas.peliculasapp.infrastructure.entities;
-    import com.peliculas.peliculasapp.domain.models.ProductionCountries;
-    import jakarta.persistence.Entity;
-    import jakarta.persistence.GeneratedValue;
-    import jakarta.persistence.GenerationType;
-    import jakarta.persistence.Id;
+package com.peliculas.peliculasapp.infrastructure.entities;
+import com.peliculas.peliculasapp.domain.models.ProductionCountries;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+@Entity
+public class ProductionCountriesEntity {
+     @Id
+     @GeneratedValue(strategy = GenerationType.IDENTITY)
+     private long id;
+     private String name;
+     private String iso31661;
+     public ProductionCountriesEntity() {}
 
-    @Entity
-    public class ProductionCountriesEntity {
+     public ProductionCountriesEntity(String name, String iso31661) {
+         this.name = name;
+         this.iso31661 = iso31661;
+     }
 
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private long id;
-        private String name;
-
-        private String iso31661;
 
     public static ProductionCountriesEntity fromDomainModel(ProductionCountries productionCountries) {
-        return new ProductionCountriesEntity();
+        return new ProductionCountriesEntity(
+                productionCountries.getName(),
+                productionCountries.getIso_3166_1()
+        );
     }
 
     public ProductionCountries toDomainModel() {
-        return new ProductionCountries();
+        return new ProductionCountries(
+                name,
+                iso31661
+        );
     }
 
 }
