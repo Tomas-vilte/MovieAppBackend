7c93342c34ceb048e10f920a7c3e22b145223fc3
Thomas <viltetomas2003@gmail.com>
2024-03-27 21:20:18 -0300

    implement mapper SpokenLanguagesEntityMapper

--- peliculas-app/src/main/java/com/peliculas/peliculasapp/infrastructure/entities/SpokenLanguagesEntity.java
+++ peliculas-app/src/main/java/com/peliculas/peliculasapp/infrastructure/entities/SpokenLanguagesEntity.java
@@ -1,11 +1,14 @@
 package com.peliculas.peliculasapp.infrastructure.entities;
-import com.peliculas.peliculasapp.domain.models.SpokenLanguages;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class SpokenLanguagesEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -14,28 +17,11 @@ public class SpokenLanguagesEntity {
     private String iso_639_1;
     private String name;
 
+    public SpokenLanguagesEntity() {}
+
     public SpokenLanguagesEntity(String english_name, String iso_639_1, String name) {
         this.english_name = english_name;
         this.iso_639_1 = iso_639_1;
         this.name = name;
-    }
-
-    public SpokenLanguages toDomainModel() {
-        return new SpokenLanguages(
-                english_name,
-                iso_639_1,
-                name
-        );
-    }
-
-    public SpokenLanguagesEntity() {}
-
-
-    public static SpokenLanguagesEntity fromDomainModel(SpokenLanguages spokenLanguages) {
-        return new SpokenLanguagesEntity(
-                spokenLanguages.getEnglish_name(),
-                spokenLanguages.getIso_639_1(),
-                spokenLanguages.getName()
-        );
     }
 }
